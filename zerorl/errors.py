db63556ff2b669b0c0a89c4dc044c79cff8e9b81
Dar-rius <mohamedtine17@gmail.com>
2026-08-08 19:30:00 +0000

    feat(erros): add exception for key not found in buffer data

--- zerorl/errors.py
+++ zerorl/errors.py
@@ -24,3 +24,25 @@ class EmptyBufferError(Exception):
         details += f" the minimal size required is: {self.require_buffer_size}\n"
         details += f"{suggestion}"
         return details
+
+
+class KeyBufferError(Exception):
+    """Raised when the argument name don't exist in data buffer.
+
+    Attributes:
+        arg_name: the argument name.
+        data_buffer: the data buffer
+    """
+
+    def __init__(self, arg_name: str, data_buffer: dict[str, object]):
+        self.arg_name = arg_name
+        self.data_buffer = data_buffer 
+
+    def __str__(self) -> str:
+        """Return a detailed error message with context and suggestion."""
+        details = f"Key '{self.arg_name}' (returned by agent.get_action) does not exist in the Buffer. \n"
+        details += f"Please ensure your Buffer is initialized with the key '{self.arg_name}\n"
+        details += f"Current valid keys are: {list(self.data_buffer.keys())}"
+        return details
+
+
